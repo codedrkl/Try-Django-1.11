934a628c3acf63089dc499ff85152b2cd6215994
kehindelawal <omokeyinde@yahoo.com>
2017-08-24 02:55:03 +0100

    mod allowed host
    modified allowed host

--- src/muypicky/old_settings.py
+++ src/muypicky/old_settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'j2b_z(*4w+#)t^nz3)0nq(tcj&3##klo73m76(x7%3z)b%85n!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.2.1.3', 'brainiac']
 
 
 # Application definition
--- src/muypicky/settings/production.py
+++ src/muypicky/settings/production.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'j2b_z(*4w+#)t^nz3)0n3da(tcj&3##klo73m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['muy-picky.herokuapp.com','.muypicky.com']
+ALLOWED_HOSTS = ['muy-picky.herokuapp.com','.muypicky.com', '10.2.1.3', 'brainiac']
 
 
 # Application definition
